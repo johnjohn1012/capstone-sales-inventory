72151381b638364def840c405a3bbfe160516992
johnjohn1012 <alexlynquinto84@gmail.com>
2025-02-10 15:29:35 +0800

    save

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 <?php 
-	$con = mysqli_connect('localhost','root','','sales_inventory_system');
+	$con = mysqli_connect('localhost','root','','sales_inventory_system','3307');
 	if (!$con) {
 		echo "Database Not Connected";
 	}
@@ -8,15 +7,3 @@
 		echo"";
 	}
  ?>
-=======
-<?php
-
-$con = mysqli_connect('localhost', 'root', '', 'sales_inventory_system','3307');
-
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "";
-
-?>
->>>>>>> c51e4fd03bd979e2ba3b0907f4dcc76478822920
