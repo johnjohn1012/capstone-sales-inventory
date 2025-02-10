e31a3f7daa4ab68413aa82c67afc2ef5818e57e9
johnjohn1012 <alexlynquinto84@gmail.com>
2025-02-10 22:37:12 +0800

    save

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="shortcut icon" type="image/png" href="images/logo.png" />
+    <link rel="shortcut icon" type="image/png" href="images/logos.png" />
     <title>Harah Rubina Del Dios </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"> <!-- Bootstrap CSS -->
     <!-- Add SweetAlert2 CSS -->
